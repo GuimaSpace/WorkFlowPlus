bf0d21bd5097e64d5f2a164fd02cf2193704656f
Vinicius Guimaraes <69005421+GuimaSpace@users.noreply.github.com>
2023-03-03 15:10:19 -0300

    Delete pages directory

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/Window.css'; 
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
-import { useRef, useState } from 'react';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-//Post UserAPI Rest 02/03/23
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-export default async function createUser(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({Status: "ok"})
-}
